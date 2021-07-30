70c9d58d1f52f24a417247f67aacd84f6da07839
Sagar Nandy <82097180+AbstractDil@users.noreply.github.com>
2021-07-30 11:30:00 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+
+function myFunction()
+{
+  var Un = document.forms["myForm"]["Uname"].value;
+  var Pw = document.forms["myForm"]["Pass"].value;
+  if(Un=="Sagar" &&  Pw=="2019")
+  {
+	window.location.href="home.html";
+  }
+  else{
+	alert("Invalid Username or Password");
+  }
+}
+
