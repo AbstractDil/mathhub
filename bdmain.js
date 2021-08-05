666e9096f8a1f1e754919b7df5a501e27edf7a95
Sagar Nandy <82097180+AbstractDil@users.noreply.github.com>
2021-08-05 09:55:28 -0700

    Update bdmain.js

--- bdmain.js
+++ bdmain.js
@@ -5,7 +5,7 @@ function myFunction()
   var Pw = document.forms["myForm"]["Pass"].value;
   if(Un=="Taal@2021" &&  Pw=="Taal@2021")
   {
-	window.location.href="waiting3.html";
+	window.location.href="Waiting3.html";
   }
   else{
 	alert("Invalid Username or Password");
