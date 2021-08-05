8965b924192fb8622c04711a9ace68f4ebe1a8c6
Sagar Nandy <82097180+AbstractDil@users.noreply.github.com>
2021-08-05 09:46:18 -0700

    Add files via upload

---
+++ bdmain.js
@@ -0,0 +1,14 @@
+
+function myFunction()
+{
+  var Un = document.forms["myForm"]["Uname"].value;
+  var Pw = document.forms["myForm"]["Pass"].value;
+  if(Un=="Taal@2021" &&  Pw=="Taal@2021")
+  {
+	window.location.href="waiting3.html";
+  }
+  else{
+	alert("Invalid Username or Password");
+  }
+}
+
