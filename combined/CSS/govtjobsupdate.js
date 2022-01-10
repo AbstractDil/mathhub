c2e112a4b72da921dba923e14586a33312191bc1
Sagar Nandy <82097180+AbstractDil@users.noreply.github.com>
2022-01-11 01:46:14 +0530

    Create govtjobsupdate.js

---
+++ combined/CSS/govtjobsupdate.js
@@ -0,0 +1,115 @@
+function SscCgle(){
+    const sscconfirmation = confirm (" This link redirected you  to official home page of SSC (https://ssc.nic.in)\n Are you sure open this external link ?");
+    if(sscconfirmation == true)
+    {
+        window.open("https://ssc.nic.in/","_blank  ", "toolbar=yes,scrollbars=yes,resizable=yes,left=150,width=1080,height=720");
+        return true;
+    }
+    else{
+        swal("Redirection Failed","Because you clicked 'Cancel' button.","error");
+        return false;
+    }
+}
+function SscCgleReg(){
+    const sscconfirmation = confirm (" This link redirected you  to the registration page of SSC (https://ssc.nic.in)\n Are you sure open this external link ?");
+    if(sscconfirmation == true)
+    {
+        window.open("https://ssc.nic.in/Registration/Home","_blank  ", "toolbar=yes,scrollbars=yes,resizable=yes,left=150,width=1080,height=720");
+        return true;
+    }
+    else{
+        swal("Redirection Failed","Because you clicked 'Cancel' button.","error");
+        return false;
+    }
+}
+function JCI(){
+    const sscconfirmation = confirm (" This link redirected you to Jute Corporation of India Ltd. \n (https://jutecorp.in)\n Are you sure open this external link ?");
+    if(sscconfirmation == true)
+    {
+        window.open("https://www.jutecorp.in/recruitment/","_blank  ", "toolbar=yes,scrollbars=yes,resizable=yes,left=150,width=1080,height=720");
+        return true;
+    }
+    else{
+        swal("Redirection Failed","Because you clicked 'Cancel' button.","error");
+        return false;
+    }
+}
+function JCIreg(){
+    const sscconfirmation = confirm (" This link redirected you to the registration page of  Jute Corporation of India Ltd. \n (https://jutecorp.in)\n Are you sure open this external link ?");
+    if(sscconfirmation == true)
+    {
+        window.open("https://jci.onlineregistrationform.org/JCI/LoginAction_registerCandidate.action?","_blank  ", "toolbar=yes,scrollbars=yes,resizable=yes,left=150,width=1080,height=720");
+        return true;
+    }
+    else{
+        swal("Redirection Failed","Because you clicked 'Cancel' button.","error");
+        return false;
+    }
+}
+function JCIaply(){
+    const sscconfirmation = confirm (" This link redirected you to the Applicant Login page of  Jute Corporation of India Ltd. \n (https://jutecorp.in)\n Are you sure open this external link ?");
+    if(sscconfirmation == true)
+    {
+        window.open("https://jci.onlineregistrationform.org/JCI/LoginAction_input.action","_blank  ", "toolbar=yes,scrollbars=yes,resizable=yes,left=150,width=1080,height=720");
+        return true;
+    }
+    else{
+        swal("Redirection Failed","Because you clicked 'Cancel' button.","error");
+        return false;
+    }
+}
+function eSIc(){
+    const sscconfirmation = confirm (" This link redirected you to the home page of ESIC. \n (https://www.esic.nic.in/)\n Are you sure open this external link ?");
+    if(sscconfirmation == true)
+    {
+        window.open("https://www.esic.nic.in/","_blank  ", "toolbar=yes,scrollbars=yes,resizable=yes,left=150,width=1080,height=720");
+        return true;
+    }
+    else{
+       swal("Redirection Failed","Because you clicked 'Cancel' button.","error");
+        return false;
+    }
+}
+function erroralrt(){
+    swal("Link is not found"," ** Please Note :  Link is not provided yet. Visit official website for more info.","error");
+  }
+  function rvwatch(){
+    const sscconfirmation = confirm (" This link redirected you to YouTube.\n Are you sure open this external link ?");
+    if(sscconfirmation == true)
+    {
+        window.open("https://youtube.com/playlist?list=PLoZP2WsNfBSGl1lp9t5wTJXHoww8BvGhq","_blank  ", "toolbar=yes,scrollbars=yes,resizable=yes,left=150,width=1080,height=720");
+        return true;
+    }
+    else{
+       swal("Redirection Failed","Because you clicked 'Cancel' button.","error");
+        return false;
+    }
+}
+
+function hindu(){
+    const sscconfirmation = confirm (" This link redirected you to  DailyEpaper webpage.\n Are you sure open this external link ?");
+    if(sscconfirmation == true)
+    {
+        window.open("https://dailyepaper.in/home-point/","_blank  ", "toolbar=yes,scrollbars=yes,resizable=yes,left=150,width=1080,height=720");
+        return true;
+    }
+    else{
+       swal("Redirection Failed","Because you clicked 'Cancel' button.","error");
+        return false;
+    }
+}
+function wordPwer(){
+    const sscconfirmation = confirm (" This link redirected you to YouTube.\n Are you sure open this external link ?");
+    if(sscconfirmation == true)
+    {
+        window.open("https://youtube.com/playlist?list=PLhJT4Hx6LtqrWwyWzBVGv7IjbdrOq12NU","_blank  ", "toolbar=yes,scrollbars=yes,resizable=yes,left=150,width=1080,height=720");
+        return true;
+    }
+    else{
+       swal("Redirection Failed","Because you clicked 'Cancel' button.","error");
+        return false;
+    }
+}
+function about(){
+    swal( "About me" , "   Hi dear , Myself Sagar Nandy. I am a student of mathematics honous. I am learning web development as my interest. I am just new to the web development and learn basics of web development. I love to make websites ❤. If you want to know more about me then contact me by filling out the contact form with valid data. \n\n 'Dhoke Ne Tere Haame Coding Sikha Diya' - Sagar Nandy." );
+  }
