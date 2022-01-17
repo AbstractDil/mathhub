68e96a79812a736a6a24cfe0a966ebb7bf525b8e
Sagar Nandy <82097180+AbstractDil@users.noreply.github.com>
2022-01-17 13:51:10 +0530

    Update govtjobsupdate.js

--- combined/CSS/govtjobsupdate.js
+++ combined/CSS/govtjobsupdate.js
@@ -193,3 +193,88 @@ function dForm(){
                 return false;
             }
         }
+// updated on 17.01.2022 starts
+        function Thanks(){
+            swal( "Thanks for visiting my website.","");
+          }
+// CSIR Jammu Junior Secretariat Assistant 2022 (advt no - 29/2021)
+//CSIR Official Link starts
+function CSIROfficalSite(){
+    const confirmation = confirm (" ** Attention : This link redirected you to the official website of CSIR Indian Institute Of Integrative Medicine. \n Are you sure to open this external link ? ");
+    if(confirmation == true)
+    {
+        window.open("https://iiim.res.in/","_blank");
+        return true;
+    }
+    else{
+       swal("Redirection Failed","Because you clicked 'Cancel' button.","error");
+        return false;
+    }
+}
+// CSIR Official Link ends
+
+// CSIR Official Notification Page Starts 
+function CSIROfficalAdvt(){
+    const confirmation = confirm (" ** Attention : This link redirected you to the advertisement  page of CSIR Indian Institute Of Integrative Medicine. \n Are you sure to open this external link ? ");
+    if(confirmation == true)
+    {
+        window.open("https://iiim.res.in/%f0%9f%85%bd%f0%9f%85%b4%f0%9f%86%86%f0%9f%91%89notification-of-advertisement-no-29-2021-for-the-posts-of-junior-secretariat-assistantsjsa/","_blank");
+        return true;
+    }
+    else{
+       swal("Redirection Failed","Because you clicked 'Cancel' button.","error");
+        return false;
+    }
+}
+// CSIR Official notification page ends
+ 
+// CSIR Official Application Portal 
+function CSIROfficalApply(){
+    const confirmation = confirm (" ** Attention : This link redirected you to the application portal of CSIR Indian Institute Of Integrative Medicine. \n Are you sure to open this external link ? ");
+    if(confirmation == true)
+    {
+        window.open("https://recruit.iiim.res.in:8081/","_blank");
+        return true;
+    }
+    else{
+       swal("Redirection Failed","Because you clicked 'Cancel' button.","error");
+        return false;
+    }
+}
+ // CSIR Official Application portal ends 
+// CSIR Official Registration Portal 
+function CSIROfficalReg(){
+    const confirmation = confirm (" ** Attention : This link redirected you to the registration page of CSIR Indian Institute Of Integrative Medicine. \n Are you sure to open this external link ? ");
+    if(confirmation == true)
+    {
+        window.open("https://recruit.iiim.res.in:8081/ApplyPost.aspx","_blank");
+        return true;
+    }
+    else{
+       swal("Redirection Failed","Because you clicked 'Cancel' button.","error");
+        return false;
+    }
+}
+ // CSIR Official  Registration portal ends 
+
+ // CSIR Official registered candidate login Portal 
+function CSIROfficalLogin(){
+    const confirmation = confirm (" ** Attention : This link redirected you to the registered candidate login page of CSIR Indian Institute Of Integrative Medicine. \n Are you sure to open this external link ? ");
+    if(confirmation == true)
+    {
+        window.open("https://recruit.iiim.res.in:8081/login.aspx","_blank");
+        return true;
+    }
+    else{
+       swal("Redirection Failed","Because you clicked 'Cancel' button.","error");
+        return false;
+    }
+}
+
+//syllabus 2022
+function syllabusCSIR(){
+    swal( "CSIR Jr. Secretariat Assistant Syllabus","Mental Ability Test - 100 Questions - 200 Marks(2 marks for every correct answer). - No Negative Marking \n\n  General Awareness & English Language (50 Questions) - 150 (3 marks for every correct answer) -  one negative mark for every wrong answer \n\n ***Mental Ability Test consisting of General Intelligence, Quantitative Aptitude, Reasoning, Problem Solving, Situational Judgment, etc. \n The Paper II will be evaluated onlv for those candidates who secure minimum threshold marks (to be determined by the Selection Committee) in the Paper I",);
+  }
+  
+ // CSIR Official  Registered candidate login  portal ends 
+ // updated on 17.01.2022 ends
