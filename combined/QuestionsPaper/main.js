0e4650d6eae27b36529b7d00d5e0c264a425da7c
Sagar Nandy <82097180+AbstractDil@users.noreply.github.com>
2022-03-21 16:43:28 +0530

    Add files via upload

---
+++ combined/QuestionsPaper/main.js
@@ -0,0 +1,68 @@
+// disable right click 
+document.addEventListener("contextmenu", function(event){
+    event.preventDefault();
+    swal("Right Click is Disabled By Administrator.","","warning");    
+    }, false);
+
+
+    document.onkeydown = function (e) {
+  
+        // disable F12 key
+        if(e.keyCode == 123) 
+        {
+            swal(" F12 Key is Disabled By Administrator.","","warning"); 
+            return false;
+        }
+ 
+        // disable I key
+        if(e.ctrlKey && e.shiftKey && e.keyCode == 73){
+            swal(" CTRL+SHIFT+I Key is Disabled By Administrator.","","warning"); 
+            return false;
+        }
+ 
+        // disable J key
+        if(e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+            swal(" CTRL+SHIFT+J Key is Disabled By Administrator.","","warning"); 
+            return false;
+        }
+ 
+        // disable U key
+        if(e.ctrlKey && e.keyCode == 85) {
+            swal(" CTRL+U Key is Disabled By Administrator.","","warning"); 
+            return false;
+        }
+        // disable F3 key
+        if(e.ctrlKey  && e.shiftKey && e.keyCode == 114) {
+            swal(" F3 Key is Disabled By Administrator.","","warning"); 
+            return false;
+        } 
+        // disable R key
+        if(e.ctrlKey  && e.shiftKey && e.keyCode == 82) {
+            swal(" CTRL+SHIFT+R Key is Disabled By Administrator.","","warning"); 
+            return false;
+        }
+        if(e.ctrlKey && e.keyCode == 82) {
+            swal(" CTRL+R Key is Disabled By Administrator.","","warning"); 
+            return false;
+        }
+        //f5 disable
+        if(e.keyCode == 116) {
+            swal(" F5 Key is Disabled By Administrator.","","warning"); 
+            return false;
+        }
+        //A disable 
+        if(e.ctrlKey && e.keyCode == 65) {
+            swal(" CTRL+A Key is Disabled By Administrator.","","warning"); 
+            return false;
+        }
+        
+    }
+ $('body').bind('copy paste',function(e) {
+    e.preventDefault(); 
+    swal(" Copy & Paste Functionality is Disabled By Administrator.","","warning"); 
+    return false; 
+});
+
+
+  
+   
