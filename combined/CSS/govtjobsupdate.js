24a44f146c3a914903eab75dec3963c56f39577c
Sagar Nandy <82097180+AbstractDil@users.noreply.github.com>
2022-03-22 23:33:37 +0530

    Update govtjobsupdate.js

--- combined/CSS/govtjobsupdate.js
+++ combined/CSS/govtjobsupdate.js
@@ -1,4 +1,5 @@
-function SscCgle(){
+// ssc cgle official link
+function SscMts(){
     const sscconfirmation = confirm (" This link redirected you  to official home page of SSC (https://ssc.nic.in)\n Are you sure open this external link ?");
     if(sscconfirmation == true)
     {
@@ -10,7 +11,8 @@ function SscCgle(){
         return false;
     }
 }
-function SscCgleReg(){
+// ssc cgle registration link
+function SscReg(){
     const sscconfirmation = confirm (" This link redirected you  to the registration page of SSC (https://ssc.nic.in)\n Are you sure open this external link ?");
     if(sscconfirmation == true)
     {
@@ -22,47 +24,16 @@ function SscCgleReg(){
         return false;
     }
 }
-function JCI(){
-    const sscconfirmation = confirm (" This link redirected you to Jute Corporation of India Ltd. \n (https://jutecorp.in)\n Are you sure open this external link ?");
-    if(sscconfirmation == true)
+
+
+
+// wbcs 2022 and Login 
+
+function WbCs(){
+    const confirmation = confirm (" This link redirected you to the home page of WBCS . \n (http://wbpsc.gov.in) \n Are you sure open this external link ?");
+    if(confirmation == true)
     {
-        window.open("https://www.jutecorp.in/recruitment/","_blank  ", "toolbar=yes,scrollbars=yes,resizable=yes,left=150,width=1080,height=720");
-        return true;
-    }
-    else{
-        swal("Redirection Failed","Because you clicked 'Cancel' button.","error");
-        return false;
-    }
-}
-function eSicReg(){
-    const sscconfirmation = confirm (" This link redirected you to the registration page of  IBPS.(https://ibpsonline.ibps.in/esiccsmdec21/basic_details.php) \n \n Are you sure open this external link ?");
-    if(sscconfirmation == true)
-    {
-        window.open("https://ibpsonline.ibps.in/esiccsmdec21/basic_details.php","_blank  ", "toolbar=yes,scrollbars=yes,resizable=yes,left=150,width=1080,height=720");
-        return true;
-    }
-    else{
-        swal("Redirection Failed","Because you clicked 'Cancel' button.","error");
-        return false;
-    }
-}
-function eSicApply(){
-    const sscconfirmation = confirm (" This link redirected you to the application page of ibps.(https://ibpsonline.ibps.in)\n Are you sure open this external link ?");
-    if(sscconfirmation == true)
-    {
-        window.open("https://ibpsonline.ibps.in/esiccsmdec21/","_blank  ", "toolbar=yes,scrollbars=yes,resizable=yes,left=150,width=1080,height=720");
-        return true;
-    }
-    else{
-        swal("Redirection Failed","Because you clicked 'Cancel' button.","error");
-        return false;
-    }
-}
-function eSIc(){
-    const sscconfirmation = confirm (" This link redirected you to the home page of ESIC. \n (https://www.esic.nic.in/)\n Are you sure open this external link ?");
-    if(sscconfirmation == true)
-    {
-        window.open("https://www.esic.nic.in/","_blank  ", "toolbar=yes,scrollbars=yes,resizable=yes,left=150,width=1080,height=720");
+        window.open("http://wbpsc.gov.in/","_blank  ", "toolbar=yes,scrollbars=yes,resizable=yes,left=150,width=1080,height=720");
         return true;
     }
     else{
@@ -70,8 +41,37 @@ function eSIc(){
         return false;
     }
 }
+
+// WBCS One time registration link
+
+function WbCsOTReg(){
+    const confirmation = confirm (" This link redirected you to the registration page of WBPSC. \n (https://wbpsc.gov.in/candidateOTRegistration.jsp)\n Are you sure open this external link ?");
+    if(confirmation == true)
+    {
+        window.open("https://wbpsc.gov.in/candidateOTRegistration.jsp","_blank  ", "toolbar=yes,scrollbars=yes,resizable=yes,left=150,width=1080,height=720");
+        return true;
+    }
+    else{
+        swal("Redirection Failed","Because you clicked 'Cancel' button.","error");
+        return false;
+    }
+}
+
+function JCIaply(){
+    const sscconfirmation = confirm (" This link redirected you to the Applicant Login page of  Jute Corporation of India Ltd. \n (https://jutecorp.in)\n Are you sure open this external link ?");
+    if(sscconfirmation == true)
+    {
+        window.open("https://jci.onlineregistrationform.org/JCI/LoginAction_input.action","_blank  ", "toolbar=yes,scrollbars=yes,resizable=yes,left=150,width=1080,height=720");
+        return true;
+    }
+    else{
+        swal("Redirection Failed","Because you clicked 'Cancel' button.","error");
+        return false;
+    }
+}
+
 function erroralrt(){
-    swal("Link Is Not Found"," ** Please Note :  Link is not provided yet or, the link is expired.","error");
+    swal("Link is not found"," ** Please Note :  Link is not provided yet. Visit official website for more info.","error");
   }
   function rvwatch(){
     const sscconfirmation = confirm (" This link redirected you to YouTube.\n Are you sure open this external link ?");
@@ -110,6 +110,7 @@ function wordPwer(){
         return false;
     }
 }
+
 
   function mDdown(){
     const sscconfirmation = confirm (" This link redirected you to the download page of Dictionary - Merriam-Webster of Google PlayStore .\n Are you sure open this external link ?");
@@ -150,8 +151,8 @@ function wordPwer(){
                 return false;
             }
         }
-        
- function eBook(){
+
+        function eBook(){
             const sscconfirmation = confirm (" ** Attention : If you don't have password, you cann't access this content. \n\n ** Password Hint : Contact the owner of this site.");
             if(sscconfirmation == true)
             {
@@ -163,13 +164,12 @@ function wordPwer(){
                 return false;
             }
         }
+        
+        function about(){
+            swal( "About me" , "   Hi dear , Myself Sagar Nandy. I am a student of mathematics honous. I am learning web development as my interest. I am just new to the web development and learn basics of web development. I love to make websites ❤. If you want to know more about me then contact me by filling out the contact form with valid data. \n\n 'Dhoke Ne Tere Haame Coding Sikha Diya' - Sagar Nandy." );
+          }
 
-
-function about(){
-    swal( "About me" , "   Hi dear , Myself Sagar Nandy. I am a student of mathematics honous. I am learning web development as my interest. I am just new to the web development and learn basics of web development. I love to make websites ❤. If you want to know more about me then contact me by filling out the contact form with valid data. \n\n 'Dhoke Ne Tere Haame Coding Sikha Diya' - Sagar Nandy." );
-  }
-
-function dForm(){
+          function dForm(){
             const sscconfirmation = confirm (" ** Attention : This link redirected you to dance form webpage of cracku.in \n Are you sure to open this external link ? ");
             if(sscconfirmation == true)
             {
@@ -182,7 +182,7 @@ function dForm(){
             }
         }
         function dFormPdf(){
-            const sscconfirmation = confirm ("  This link redirected you to dance form webpage of cracku.in \n Are you sure to open this external link ?");
+            const sscconfirmation = confirm (" ** Attention : If you don't have password, you cann't access this content. \n\n ** Password Hint : Contact the owner of this site.");
             if(sscconfirmation == true)
             {
                 window.open("https://cracku.in/downloads/231","_blank");
