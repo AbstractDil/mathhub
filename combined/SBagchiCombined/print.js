e5c488b4c0f918f1c18a8b592ff2ca75560066e3
Sagar Nandy <82097180+AbstractDil@users.noreply.github.com>
2022-07-06 21:29:55 +0530

    Update print.js

--- combined/SBagchiCombined/print.js
+++ combined/SBagchiCombined/print.js
@@ -266,24 +266,5 @@ document.addEventListener("contextmenu", function(event){
     swal(" Copy & Paste Functionality is Disabled By Administrator.","","warning"); 
     return false; 
 });
-const Username = prompt("Enter Username");
-        if(Username == "Sagar2021")
-        {
-       const Password = prompt("Enter Password");
-        
-        if(Password=="202120222035s"){
-            alert("Access Granted");
-            // window.location="index.html";
 
-        }
-        else
-        {
-            alert("Incorrect Password.");
-            window.location="error.html"
-    }
-}
-else{
-    alert("Access Denied.")
-    window.location="https://nandysagar.in/combined/error.html"
-}
 
